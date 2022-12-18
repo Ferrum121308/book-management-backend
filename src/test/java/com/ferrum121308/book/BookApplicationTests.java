c27610f523a05653420812438af6eefcbb235bd3
Luka James <ferrum121308@163.com>
2022-12-19 02:33:09 +0800

    图书管理系统代码初始化

---
+++ src/test/java/com/ferrum121308/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferrum121308.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
